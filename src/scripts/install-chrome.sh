c35eb2f3dbdcc9cde916626edf7eefc50c631435
KyleTryon <Kyle.Tryon@circleci.com>
2022-04-11 14:00:22 -0400

    refactor: Orb Tools 11 + migrate bash scripts

---
+++ src/scripts/install-chrome.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+# installation check
+if uname -a | grep Darwin >/dev/null 2>&1; then
+  if ls /Applications | grep "Google Chrome" >/dev/null 2>&1; then
+    if [ "$ORB_PARAM_REPLACE_EXISTING" == "1" ]; then
+      echo "$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version)is currently installed; replacing it"
+      HOMEBREW_NO_AUTO_UPDATE=1 brew uninstall google-chrome >/dev/null 2>&1 || true
+      $SUDO rm -rf /Applications/Google\ Chrome.app >/dev/null 2>&1 || true
+    else
+      echo "$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version)is already installed"
+      exit 0
+    fi
+  else
+    echo "Google Chrome is not currently installed; installing it"
+  fi
+elif cat /etc/issue | grep Alpine >/dev/null 2>&1; then
+  if command -v chromium-browser >/dev/null 2>&1; then
+    if [ "$ORB_PARAM_REPLACE_EXISTING" == "1" ]; then
+      echo "$(chromium-browser --version)is currently installed; replacing it"
+      $SUDO apk del --force-broken-world chromium >/dev/null 2>&1 || true
+      $SUDO rm -f "$(command -v chromium-browser)" >/dev/null 2>&1 || true
+    else
+      echo "$(chromium-browser --version)is already installed to $(which chromium-browser)"
+      exit 0
+    fi
+  else
+    echo "Google Chrome (via Chromium) is not currently installed; installing it"
+  fi
+elif command -v yum >/dev/null 2>&1; then
+  if command -v google-chrome >/dev/null 2>&1; then
+    if [ "$ORB_PARAM_REPLACE_EXISTING" == "1" ]; then
+      echo "$(google-chrome --version)is currently installed; replacing it"
+      $SUDO yum remove -y google-chrome-stable >/dev/null 2>&1 || true
+      $SUDO rm -f "$(command -v google-chrome)" >/dev/null 2>&1 || true
+    else
+      echo "$(google-chrome --version)is already installed to $(which google-chrome)"
+      exit 0
+    fi
+  else
+    echo "Google Chrome is not currently installed; installing it"
+  fi
+else
+  if command -v google-chrome >/dev/null 2>&1; then
+    if [ "$ORB_PARAM_REPLACE_EXISTING" == "1" ]; then
+      echo "$(google-chrome --version)is currently installed; replacing it"
+      $SUDO apt-get -y --purge remove google-chrome-stable >/dev/null 2>&1 || true
+      $SUDO rm -f "$(command -v google-chrome)" >/dev/null 2>&1 || true
+    else
+      echo "$(google-chrome --version)is already installed to $(which google-chrome)"
+      exit 0
+    fi
+  else
+    echo "Google Chrome is not currently installed; installing it"
+  fi
+fi
+
+# install chrome
+if uname -a | grep Darwin > /dev/null 2>&1; then
+  brew update > /dev/null 2>&1 && \
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install google-chrome > /dev/null 2>&1
+  echo -e "#\!/bin/bash\n" > google-chrome
+  perl -i -pe "s|#\\\|#|g" google-chrome
+  echo -e "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \"\$@\"" >> google-chrome
+  $SUDO mv google-chrome /usr/local/bin
+  $SUDO chmod +x /usr/local/bin/google-chrome
+  # test/verify installation
+  if google-chrome --version > /dev/null 2>&1; then
+    echo "$(google-chrome --version)has been installed in the /Applications directory"
+    echo "A shortcut has also been created at $(which google-chrome)"
+    exit 0
+  else
+    echo "Something went wrong; Google Chrome could not be installed"
+    exit 1
+  fi
+elif command -v yum > /dev/null 2>&1; then
+  # download chrome
+  if [[ "$ORB_PARAM_CHROME_VERSION" == "latest" ]]; then
+    CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm"
+  else
+    CHROME_URL="https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-$ORB_PARAM_CHROME_VERSION-1.x86_64.rpm"
+  fi
+  curl --silent --show-error --location --fail --retry 3 \
+    --output google-chrome.rpm \
+    $CHROME_URL
+  curl --silent --show-error --location --fail --retry 3 \
+    --output liberation-fonts.rpm \
+    http://mirror.centos.org/centos/7/os/x86_64/Packages/liberation-fonts-1.07.2-16.el7.noarch.rpm
+  $SUDO yum localinstall -y liberation-fonts.rpm \
+    > /dev/null 2>&1
+  $SUDO yum localinstall -y google-chrome.rpm \
+    > /dev/null 2>&1
+  rm -rf google-chrome.rpm liberation-fonts.rpm
+else
+  # download chrome
+  if [[ "$ORB_PARAM_CHROME_VERSION" == "latest" ]]; then
+    CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+  else
+    CHROME_URL="https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${ORB_PARAM_CHROME_VERSION}-1_amd64.deb"
+  fi
+  curl --silent --show-error --location --fail --compressed \
+    --retry 3 --retry-delay 5 --keepalive-time 2 \
+    --output google-chrome.deb $CHROME_URL
+  # Debian 10 fix
+  if grep -i buster /etc/os-release;then
+    $SUDO apt-get --allow-releaseinfo-change-suite update
+  fi
+  # Ensure that Chrome apt dependencies are installed.
+  #$SUDO apt-get update
+  # The pipe will install any dependencies missing
+  $SUDO dpkg -i google-chrome.deb || $SUDO apt-get update && $SUDO apt-get -fy install
+  rm -rf google-chrome.deb
+  $SUDO sed -i 's|HERE/chrome"|HERE/chrome" --disable-setuid-sandbox --no-sandbox|g' "/opt/google/chrome/google-chrome"
+fi
+
+# test/verify installation
+if [[ "$ORB_PARAM_CHROME_VERSION" != "latest" ]]; then
+  if google-chrome --version | grep "$ORB_PARAM_CHROME_VERSION" > /dev/null 2>&1; then
+    :
+  else
+    echo "Something went wrong; Google Chrome could not be installed"
+    exit 1
+  fi
+else
+  if google-chrome --version  > /dev/null 2>&1; then
+    :
+  else
+    echo "Something went wrong; Google Chrome could not be installed"
+    exit 1
+  fi
+  echo "$(google-chrome --version) has been installed to $(which google-chrome)"
+  echo "Chrome: $ORB_PARAM_CHROME_VERSION" >> ${HOME}/.browser-versions
+fi
---
+++ src/scripts/install-chromedriver.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+# determine_chrome_version
+if uname -a | grep Darwin> /dev/null 2>&1; then
+  echo "System detected as MacOS"
+  CHROME_VERSION="$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version)"
+  PLATFORM=mac64
+
+elif cat /etc/issue | grep Alpine > /dev/null 2>&1; then
+  apk update > /dev/null 2>&1 && \
+    apk add --no-cache chromium-chromedriver > /dev/null
+
+  # verify version
+  echo "$(chromedriver --version) has been installed to $(which chromedriver)"
+
+  exit 0
+else
+  CHROME_VERSION="$(google-chrome --version)"
+  PLATFORM=linux64
+fi
+
+CHROME_VERSION_STRING="$(echo $CHROME_VERSION | sed 's/.*Google Chrome //' | sed 's/.*Chromium //')"
+
+# print Chrome version
+echo "Installed version of Google Chrome is $CHROME_VERSION_STRING"
+
+# determine chromedriver release
+CHROMEDRIVER_RELEASE="${CHROME_VERSION_STRING%%.*}"
+
+let CHROME_RELEASE=$CHROMEDRIVER_RELEASE
+
+if [[ $CHROME_RELEASE -lt 70 ]]; then
+  # https://sites.google.com/a/chromium.org/chromedriver/downloads
+  # https://chromedriver.storage.googleapis.com/2.40/notes.txt
+
+  case $CHROME_RELEASE in
+  69)
+    CHROMEDRIVER_VERSION="2.44";;
+  68)
+    CHROMEDRIVER_VERSION="2.42";;
+  67)
+    CHROMEDRIVER_VERSION="2.41";;
+  66)
+    CHROMEDRIVER_VERSION="2.40";;
+  65)
+    CHROMEDRIVER_VERSION="2.38";;
+  64)
+    CHROMEDRIVER_VERSION="2.37";;
+  63)
+    CHROMEDRIVER_VERSION="2.36";;
+  62)
+    CHROMEDRIVER_VERSION="2.35";;
+  61)
+    CHROMEDRIVER_VERSION="2.34";;
+  60)
+    CHROMEDRIVER_VERSION="2.33";;
+  59)
+    CHROMEDRIVER_VERSION="2.32";;
+  58)
+    CHROMEDRIVER_VERSION="2.31";;
+  [56-57])
+    CHROMEDRIVER_VERSION="2.29";;
+  55)
+    CHROMEDRIVER_VERSION="2.28";;
+  54)
+    CHROMEDRIVER_VERSION="2.27";;
+  53)
+    CHROMEDRIVER_VERSION="2.26";;
+  52)
+    CHROMEDRIVER_VERSION="2.24";;
+  51)
+    CHROMEDRIVER_VERSION="2.23";;
+  [49-50])
+    CHROMEDRIVER_VERSION="2.22";;
+  [46-48])
+    CHROMEDRIVER_VERSION="2.21";;
+  [43-45])
+    CHROMEDRIVER_VERSION="2.20";;
+  *)
+    echo "Sorry, Google Chrome/Chromium version 43 or newer is required to use ChromeDriver"
+    exit 1;;
+  esac
+else
+  CHROMEDRIVER_VERSION=$(curl --silent --show-error --location --fail --retry 3 \
+    "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROMEDRIVER_RELEASE")
+fi
+
+# installation check
+if command -v chromedriver > /dev/null 2>&1; then
+  if chromedriver --version | grep "$CHROMEDRIVER_VERSION" > /dev/null 2>&1; then
+    echo "ChromeDriver $CHROMEDRIVER_VERSION is already installed"
+    exit 0
+  else
+    echo "A different version of ChromeDriver is installed ($(chromedriver --version)); removing it"
+    $SUDO rm -f $(command -v chromedriver)
+  fi
+fi
+
+echo "ChromeDriver $CHROMEDRIVER_VERSION will be installed"
+
+# download chromedriver
+curl --silent --show-error --location --fail --retry 3 \
+  --output chromedriver_$PLATFORM.zip \
+  "http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_$PLATFORM.zip"
+
+# setup chromedriver installation
+if command -v yum > /dev/null 2>&1; then
+  yum install -y unzip > /dev/null 2>&1
+fi
+
+unzip "chromedriver_$PLATFORM.zip" > /dev/null 2>&1
+rm -rf "chromedriver_$PLATFORM.zip"
+
+$SUDO mv chromedriver "$ORB_PARAM_DRIVER_INSTALL_DIR"
+$SUDO chmod +x "$ORB_PARAM_DRIVER_INSTALL_DIR/chromedriver"
+
+# test/verify version
+if chromedriver --version | grep "$CHROMEDRIVER_VERSION" > /dev/null 2>&1; then
+  echo "$(chromedriver --version) has been installed to $(which chromedriver)"
+else
+  echo "Something went wrong; ChromeDriver could not be installed"
+  exit 1
+fi
