cd251dd2743b54bf351200019fd74b4b6210821b
KyleTryon <Kyle.Tryon@circleci.com>
2022-04-13 14:53:05 -0400

    debug: testing output

--- src/scripts/install-chrome.sh
+++ src/scripts/install-chrome.sh
@@ -64,7 +64,7 @@ if uname -a | grep Darwin >/dev/null 2>&1; then
   sudo /usr/sbin/installer -pkg "$CHROME_TEMP_DIR/googlechrome.pkg" -target /
   sudo rm -rf "$CHROME_TEMP_DIR"
   xattr -rc "/Applications/Google Chrome.app"
-  echo 'alias google-chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"' >> "$BASH_ENV"
+  echo 'alias google-chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"' >> "$BASH_ENV"
   echo 'TEST_ENV="Sourced env variable"' >> "$BASH_ENV"
   cat "$BASH_ENV"
   # shellcheck source=/dev/null
@@ -78,6 +78,7 @@ if uname -a | grep Darwin >/dev/null 2>&1; then
     echo "Failed to source $BASH_ENV"
     echo "$TEST_ENV"
   fi
+  alias
   google-chrome --version
   # test/verify installation
   if google-chrome --version >/dev/null 2>&1; then
