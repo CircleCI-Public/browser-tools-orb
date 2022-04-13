41ab7cb0cc5186364ee6a038feda37af3c72f856
KyleTryon <Kyle.Tryon@circleci.com>
2022-04-13 09:40:37 -0400

    fix: create alias to chrome bin inside the app pkg

--- src/scripts/install-chrome.sh
+++ src/scripts/install-chrome.sh
@@ -64,6 +64,8 @@ if uname -a | grep Darwin >/dev/null 2>&1; then
   sudo /usr/sbin/installer -pkg "$CHROME_TEMP_DIR/googlechrome.pkg" -target /
   sudo rm -rf "$CHROME_TEMP_DIR"
   xattr -rc "/Applications/Google Chrome.app"
+  echo 'alias google-chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"' >> $BASH_ENV
+  source $BASH_ENV
   # test/verify installation
   if google-chrome --version >/dev/null 2>&1; then
     echo "$(google-chrome --version)has been installed in the /Applications directory"
