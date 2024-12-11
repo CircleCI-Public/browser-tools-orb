1435fba4e3ffb4575f7b033b8b643cedb69cabca
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-12-11 15:53:48 -0500

    Updat test

--- src/scripts/process-cache.sh
+++ src/scripts/process-cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
-if [ -f "chrome.tar.gz" ]; then
+if [ -f "/tmp/chrome.tar.gz" ]; then
     $SUDO mkdir -p /opt/google/chrome
     $SUDO tar -xzf /tmp/chrome.tar.gz -C /opt/google/chrome
     $SUDO rm -rf /tmp/chrome.tar.gz
