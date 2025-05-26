272f6f5048607b392b4d2cb5a599cf017ceb22a5
marboledacci <mateo.arboleda@circleci.com>
2025-05-26 08:37:19 -0500

    Feat/edge browser (#132)
    * Add Edge Browser install
    
    * Fix linter
    
    * Fix install validations
    
    * Update versions to install in tests
    
    * Update versions to install in tests
    
    * Update edge driver installation for macos
    
    * Add non interactive flag

---
+++ src/scripts/install-edge-driver.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+
+if uname -a | grep Darwin >/dev/null 2>&1; then
+  if ! "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge" --version >/dev/null 2>&1; then
+    echo "Microsoft Edge is not installed"
+    exit 1
+  fi
+  PLATFORM=mac64_m1
+  VERSION=$("/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge" --version | awk '{print $3}')
+else
+  if ! command -v microsoft-edge >/dev/null 2>&1; then
+    echo "Microsoft Edge is not installed"
+    exit 1
+  fi
+  PLATFORM=linux64
+  VERSION=$(microsoft-edge --version | awk '{print $3}')
+fi
+
+
+wget -q -O edgedriver.zip "https://msedgedriver.azureedge.net/$VERSION/edgedriver_$PLATFORM.zip"
+unzip edgedriver.zip >/dev/null 2>&1
+$SUDO mv msedgedriver "$ORB_PARAM_DRIVER_INSTALL_DIR"
+rm -rf edgedriver.zip Driver_Notes
+
+$SUDO chmod +x "$ORB_PARAM_DRIVER_INSTALL_DIR/msedgedriver"
+
+if "$ORB_PARAM_DRIVER_INSTALL_DIR/msedgedriver" --version | grep "$VERSION" >/dev/null 2>&1; then
+  echo "Edge Web Driver installed correctly"
+else
+  echo "Error installing Edge Web Driver"
+  exit 1
+fi
---
+++ src/scripts/install-edge.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+
+if uname -a | grep Darwin >/dev/null 2>&1; then
+  if ! command -v brew >/dev/null 2>&1; then
+    echo "You need brew to install Edge on MacOS"
+    exit 1
+  fi
+  brew install --cask microsoft-edge
+  if "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge" --version >/dev/null 2>&1; then
+    echo "Microsoft Edge version was installed."
+  else
+    echo "Microsoft Edge could not be installed"
+    exit 1
+  fi
+elif command -v apt >/dev/null 2>&1; then
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-archive-keyring.gpg
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge-stable.list
+  if [ "$ORB_PARAM_VERSION" != "latest" ]; then
+    VERSION="=$ORB_PARAM_VERSION-1"
+  fi
+  $SUDO apt-get update
+  DEBIAN_FRONTEND=noninteractive $SUDO apt-get install -y "microsoft-edge-stable$VERSION"
+  if command -v microsoft-edge >/dev/null 2>&1; then
+    echo "Microsoft Edge version $(microsoft-edge --version) was installed."
+  else
+    echo "Microsoft Edge could not be installed"
+    exit 1
+  fi
+fi
