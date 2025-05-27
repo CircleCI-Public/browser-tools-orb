83fd24f25192f027afe12339ad168c4ced5dc51d
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-05-27 11:38:44 -0500

    add sudo to unzip files

--- src/scripts/install-chrome-for-testing.sh
+++ src/scripts/install-chrome-for-testing.sh
@@ -4,21 +4,21 @@ if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 cd "$ORB_PARAM_DIR" || exit 1
 
 if uname -a | grep Darwin >/dev/null 2>&1; then
-    wget -q -O chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/mac-arm64/chrome-mac-arm64.zip"
+    $SUDO wget -q -O chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/mac-arm64/chrome-mac-arm64.zip"
     $SUDO unzip chrome-for-testing.zip >/dev/null 2>&1
 
-    wget -q -O chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/mac-arm64/chromedriver-mac-arm64.zip"
+    $SUDO wget -q -O chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/mac-arm64/chromedriver-mac-arm64.zip"
     $SUDO unzip chrome-for-testing-driver.zip >/dev/null 2>&1
     $SUDO mv chromedriver-mac-arm64/chromedriver chromedriver
 elif command -v apt >/dev/null 2>&1; then
-    wget -q -O chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/linux64/chrome-linux64.zip"
+    $SUDO wget -q -O chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/linux64/chrome-linux64.zip"
     $SUDO unzip chrome-for-testing.zip >/dev/null 2>&1
     $SUDO apt-get update
     while read -r pkg; do
         $SUDO apt-get satisfy -y --no-install-recommends "${pkg}";
     done < chrome-linux64/deb.deps;
 
-    wget -q -O chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/linux64/chromedriver-linux64.zip"
+    $SUDO wget -q -O chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/linux64/chromedriver-linux64.zip"
     $SUDO unzip chrome-for-testing-driver.zip >/dev/null 2>&1
     $SUDO mv chromedriver-linux64/chromedriver chromedriver
 fi
