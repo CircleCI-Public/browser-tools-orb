966b25ae928ab36c177805e790bd8f6f19faf4ba
marboledacci <mateo.arboleda@circleci.com>
2025-05-28 08:26:21 -0500

    Feat/chrome for testing (#133)
    * Add chrome for testing install
    
    * Fix linter
    
    * fix tests
    
    * fix tests
    
    * add sudo to unzip files
    
    * add sudo to unzip files
    
    * Add validation for non existing versions
    
    * Remove duplicated unzip

---
+++ src/scripts/install-chrome-for-testing.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+
+cd "$ORB_PARAM_DIR" || exit 1
+
+if uname -a | grep Darwin >/dev/null 2>&1; then
+    $SUDO wget -q -O chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/mac-arm64/chrome-mac-arm64.zip"
+    if [ -s "chrome-for-testing.zip" ]; then
+        $SUDO unzip chrome-for-testing.zip >/dev/null 2>&1
+    else
+        echo "Version $ORB_PARAM_VERSION doesn't exist"
+        exit 1
+    fi
+    $SUDO wget -q -O chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/mac-arm64/chromedriver-mac-arm64.zip"
+    $SUDO unzip chrome-for-testing-driver.zip >/dev/null 2>&1
+    $SUDO mv chromedriver-mac-arm64/chromedriver chromedriver
+elif command -v apt >/dev/null 2>&1; then
+    $SUDO wget -q -O chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/linux64/chrome-linux64.zip"
+    if [ -s "chrome-for-testing.zip" ]; then
+        $SUDO unzip chrome-for-testing.zip >/dev/null 2>&1
+    else
+        echo "Version $ORB_PARAM_VERSION doesn't exist"
+        exit 1
+    fi
+    $SUDO apt-get update
+    while read -r pkg; do
+        $SUDO apt-get satisfy -y --no-install-recommends "${pkg}";
+    done < chrome-linux64/deb.deps;
+
+    $SUDO wget -q -O chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/linux64/chromedriver-linux64.zip"
+    $SUDO unzip chrome-for-testing-driver.zip >/dev/null 2>&1
+    $SUDO mv chromedriver-linux64/chromedriver chromedriver
+fi
+$SUDO chmod +x chromedriver
+
+if chromedriver --version | grep "$ORB_PARAM_VERSION" >/dev/null 2>&1; then
+  echo "Chrome for testing installed correctly"
+else
+  echo "Error installing Chrome for testing"
+  exit 1
+fi
