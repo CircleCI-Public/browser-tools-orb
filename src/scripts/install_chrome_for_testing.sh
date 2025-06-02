2ac9d86e9f2da928fdbc6dd272d48adeade12798
Bill Glesias <bglesias@gmail.com>
2025-06-02 14:50:24 -0400

    feat: allow for version to be optional and discover the latest version of Chrome for Testing and install it

--- src/scripts/install_chrome_for_testing.sh
+++ src/scripts/install_chrome_for_testing.sh
@@ -3,31 +3,50 @@ if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
 cd "$ORB_PARAM_DIR" || { echo "$ORB_PARAM_DIR does not exist. Exiting"; exit 1; }
 
-if [ -z "$ORB_PARAM_VERSION" ]; then
-    echo "ORB_PARAM_VERSION is not set. Exiting."
-    exit 1
+# process ORB_PARAM_VERSION
+if command -v circleci &>/dev/null; then
+  # CircleCI is installed, proceed with substitution
+  PROCESSED_CHROME_VERSION=$(circleci env subst "$ORB_PARAM_VERSION")
+else
+  # CircleCI is not installed, fallback to using the environment variable as-is
+  echo "CircleCI CLI is not installed. Relying on the environment variable ORB_PARAM_VERSION to be set manually."
+  PROCESSED_CHROME_VERSION=${ORB_PARAM_VERSION:-latest} # Default to "latest" if the variable is unset
 fi
 
 if uname -a | grep Darwin >/dev/null 2>&1; then
-    $SUDO curl -s -o chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/mac-arm64/chrome-mac-arm64.zip"
+    if [[ "$PROCESSED_CHROME_VERSION" == "latest" ]]; then
+      LATEST_VERSION="$(curl -s 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Mac' | jq -r ' .[0] | .version ')"
+      target_version="$LATEST_VERSION"
+    else
+      target_version="$PROCESSED_CHROME_VERSION"
+    fi
+
+    $SUDO curl -s -o chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$target_version/mac-arm64/chrome-mac-arm64.zip"
     if [ -s "chrome-for-testing.zip" ]; then
         $SUDO unzip chrome-for-testing.zip >/dev/null 2>&1
     else
-        echo "Version $ORB_PARAM_VERSION doesn't exist"
-        exit 1
+        echo "Version $target_version doesn't exist"
+        #exit 1
     fi
 
     if [ "$ORB_PARAM_INSTALL_CHROMEDRIVER" = true ] ; then
-        $SUDO curl -s -o chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/mac-arm64/chromedriver-mac-arm64.zip"
+        $SUDO curl -s -o chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$target_version/mac-arm64/chromedriver-mac-arm64.zip"
         $SUDO unzip chrome-for-testing-driver.zip >/dev/null 2>&1
         $SUDO mv chromedriver-mac-arm64/chromedriver chromedriver
     fi
 elif command -v apt >/dev/null 2>&1; then
-    $SUDO curl -s -o chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/linux64/chrome-linux64.zip"
+    if [[ "$PROCESSED_CHROME_VERSION" == "latest" ]]; then
+      LATEST_VERSION="$(curl -s 'https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Linux' | jq -r ' .[0] | .version ')"
+      target_version="$LATEST_VERSION"
+    else
+      target_version="$PROCESSED_CHROME_VERSION"
+    fi
+
+    $SUDO curl -s -o chrome-for-testing.zip "https://storage.googleapis.com/chrome-for-testing-public/$target_version/linux64/chrome-linux64.zip"
     if [ -s "chrome-for-testing.zip" ]; then
         $SUDO unzip chrome-for-testing.zip >/dev/null 2>&1
     else
-        echo "Version $ORB_PARAM_VERSION doesn't exist"
+        echo "Version $target_version doesn't exist"
         exit 1
     fi
     $SUDO apt-get update
@@ -36,7 +55,7 @@ elif command -v apt >/dev/null 2>&1; then
     done < chrome-linux64/deb.deps;
 
     if [ "$ORB_PARAM_INSTALL_CHROMEDRIVER" = true ] ; then
-        $SUDO curl -s -o chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$ORB_PARAM_VERSION/linux64/chromedriver-linux64.zip"
+        $SUDO curl -s -o chrome-for-testing-driver.zip "https://storage.googleapis.com/chrome-for-testing-public/$target_version/linux64/chromedriver-linux64.zip"
         $SUDO unzip chrome-for-testing-driver.zip >/dev/null 2>&1
         $SUDO mv chromedriver-linux64/chromedriver chromedriver
     fi
@@ -47,7 +66,7 @@ if [ "$ORB_PARAM_INSTALL_CHROMEDRIVER" = true ] ; then
 fi
 
 if [ "$ORB_PARAM_INSTALL_CHROMEDRIVER" = true ] ; then
-    if chromedriver --version | grep "$ORB_PARAM_VERSION" >/dev/null 2>&1; then
+    if chromedriver --version | grep "$target_version" >/dev/null 2>&1; then
         echo "chromedriver for Chrome for testing installed correctly"
     else
         echo "Error installing Chrome for testing"
